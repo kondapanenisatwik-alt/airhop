43687be23ea16918cac492bb52759daf2cf7a210
kondapanenisatwik-alt <kondapanenisatwik@gmail.com>
2025-09-30 21:43:29 +0530

    Delete export.js

--- export.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { exportRoute } = require('../controllers/exportController');
-
-router.post('/', exportRoute);
-
-module.exports = router;
