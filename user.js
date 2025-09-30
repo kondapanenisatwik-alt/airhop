d6cc0898b78124577d95756d3fcbff4ee2db0586
kondapanenisatwik-alt <kondapanenisatwik@gmail.com>
2025-09-30 21:40:13 +0530

    Delete user.js

--- user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { previousSearches } = require('../controllers/userController');
-
-// GET /api/user/previous-searches
-router.get('/previous-searches', previousSearches);
-
-module.exports = router;
